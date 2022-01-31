5d826f3c2a5c6f9923630fa8894951bf9403e85d
paramore21 <ilnuro4ka01@gmail.com>
2022-01-31 17:40:30 +0500

    768, 1080, 1440, 1920 resolutions created

--- script/index.js
+++ script/index.js
@@ -4,8 +4,14 @@ const langUZ = ".header__nav-item_bordered__uzb";
 const aboutYellow = ".about-us__button_yellow";
 const aboutLight = ".about-us__button_light";
 const startWorking = ".start-working__button";
+const auth = ".header__nav-item_yellow_bordered";
 
-const infoBadge = document.querySelectorAll(".info__badge-item")
+const infoBadge = document.querySelectorAll(".info__badge-item");
+
+const burgerOpenMenu = document.querySelector(".header__burger-open-icon");
+const burgerCloseMenu = document.querySelector(".header__burger-close-icon");
+
+
 function mouseHandler(btnClass) {
     let btn = document.querySelector(btnClass);
     btn.onmousemove = function (e) {
@@ -22,8 +28,9 @@ mouseHandler(langEN);
 mouseHandler(aboutLight);
 mouseHandler(aboutYellow);
 mouseHandler(startWorking);
+mouseHandler(auth);
 
-function handleToggle(e){
+function handleToggleAccordion(e){
     const button = e.querySelector(".info__badge-span_hidden")
     const image = e.querySelector(".info__badge-item-img")
     image.classList.toggle("info__badge-item-img_expanded")
@@ -32,10 +39,23 @@ function handleToggle(e){
 
 function expandSpan() {
     infoBadge.forEach((elem, index) => {
-        elem.addEventListener("click", () => handleToggle(elem))
+        elem.addEventListener("click", () => handleToggleAccordion(elem))
     })
 }
 
 expandSpan()
 
+function toggleMenu() {
+    const burgerBox = document.querySelector(".header__burger-menu")
+    const burgerIcon = document.querySelector(".header__burger-open-icon")
+    burgerBox.classList.toggle("header__burger-menu_opened")
+    burgerIcon.classList.toggle("header__burger-icon")
+}
 
+burgerOpenMenu.addEventListener("click", () => {
+    toggleMenu()
+})
+
+burgerCloseMenu.addEventListener("click", () => {
+    toggleMenu()
+})
