9a0ab0e9511f9f9e200bbf3854b8dc51048368cc
DarkLektor <darklektor1@gmail.com>
2022-08-04 13:01:03 +0500

    Main page sections 1920 completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".main-page-slider").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
     autoplayHoverPause: true,
+    dots: false,
+  });
+  $(".partners__slider").owlCarousel({
+    items: 5,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    margin: 60,
+    dots: false,
   });
 });
