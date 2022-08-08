7c8be92ccc12efa55a6e5aecd571e3885dae5fd0
DarkLektor <darklektor1@gmail.com>
2022-08-08 18:50:37 +0500

    [!] Adaptive added

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,33 @@ $(document).ready(function () {
   $(".main-page-slider").owlCarousel({
     items: 1,
     loop: true,
-    // autoplay: true,
+    autoplay: true,
     autoplayHoverPause: true,
     dots: false,
   });
+
   $(".partners__slider").owlCarousel({
-    items: 5,
+    items: 3,
     loop: true,
     autoplay: true,
     autoplayHoverPause: true,
     autoplayTimeout: 3000,
-    margin: 60,
+    margin: 16,
     dots: false,
+    responsive: {
+      1366: {
+        items: 5,
+        margin: 60,
+      },
+      900: {
+        items: 4,
+        margin: 48,
+      },
+      650: {
+        items: 3,
+        margin: 32,
+      },
+    },
   });
 
   $(".circle-animation-btn").mousemove(function (e) {
@@ -42,9 +57,20 @@ $(document).ready(function () {
   $(".header__burger").on("click", function () {
     $(".header__burger").toggleClass("active");
     $(".header__menu").toggleClass("active");
+    $(".overlay").toggleClass("active");
 
     $(".header__burger-cheeckbox").is(":checked")
       ? $(".header__burger-cheeckbox").val()
       : $(".header__burger-cheeckbox").val("on");
+  });
+
+  $(".overlay").on("click", function () {
+    $(".header__burger")[0].click();
+  });
+
+  $(window).scroll(function () {
+    $(this).scrollTop() > $(".main-page-slider").outerHeight()
+      ? $(".header").addClass("white-theme")
+      : $(".header").removeClass("white-theme");
   });
 });
