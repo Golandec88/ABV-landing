b4c5677dc12c8a022477ba362794336212d958ca
DarkLektor <darklektor1@gmail.com>
2022-11-08 14:14:18 +0500

    [WIP] Edo benefits section

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -48,6 +48,18 @@ $(document).ready(function () {
     },
   });
 
+  $(".edo-benefits__left_nav_item").on("click", function () {
+    console.log(this.classList.find((item) => item.includes("item")));
+  });
+  const edoBenefits = $(".edo-benefits__right_slider");
+  edoBenefits.owlCarousel({
+    loop: true,
+    dots: false,
+    items: 1,
+    autoHeight: true,
+    margin: 50,
+  });
+
   $(".deatailed-btn").on("click", function (e) {
     e.stopPropagation();
     const wrapper =
@@ -85,14 +97,13 @@ function showDetails(wrapper) {
   $(wrapper).find(".text__detailed").addClass("active");
   $(wrapper).find(".text__desc").removeClass("active");
   $(wrapper).find(".text__desc").addClass("hide");
-  $(".owl-carousel").trigger("refresh.owl.carousel");
 }
 function showDesc(wrapper) {
   $(wrapper).find(".buttons__item.detailed").removeClass("active");
   $(wrapper).find(".buttons__item.desc").addClass("active");
   $(wrapper).find(".text__detailed").addClass("hide");
+
   $(wrapper).find(".text__detailed").removeClass("active");
   $(wrapper).find(".text__desc").addClass("active");
   $(wrapper).find(".text__desc").removeClass("hide");
-  $(".owl-carousel").trigger("refresh.owl.carousel");
 }
